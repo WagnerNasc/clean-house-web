121cfbbd83cbdb3bbb123af4d6bd91658bc0af1c
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-01-21 12:18:57 -0300

    feat: added global styles and assets

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'blue-300': '#3C7AE5',
+  'blue-500': '#2E71E8',
+  'blue-700': '#1B1EC7',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+} as const
